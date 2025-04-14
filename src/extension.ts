390fab8c69114fba78ce00df4e75dd35657489ea
pranav <pranavbavaskar1@gmail.com>
2025-04-14 23:05:12 +0530

    basic api call to gemini

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,58 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your exension "ai-code-reviewer" is now active!'
+  );
+  console.log("GEMINI Key", process.env.GEMINI_API_KEY);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your exension "ai-code-reviewer" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "ai-code-reviewer.helloWorld",
+    async () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`;
+      const payload = {
+        contents: [
+          {
+            parts: [{ text: "Explain how AI works" }],
+          },
+        ],
+      };
+      try {
+        const res = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ai-code-reviewer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from NO reviewer!');
-	});
+        const a = await res.json();
+        vscode.window.showInformationMessage(
+          //   a.candidates.content.parts[0].text
+          JSON.stringify(a)
+        );
+      } catch (error) {
+        console.log("error", error);
+      }
 
-	context.subscriptions.push(disposable);
+      //   @ts-ignore
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
